e5868e452b661ef637782ce9b394a747ef125e7a
manuelsolaberrieta <solaberrietamanuel@gmail.com>
2022-12-23 15:12:01 -0300

    Paint only when click and mouseover

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const GRID_CONTAINER = document.getElementById("grid-container");
 const GRID_BUTTON = document.querySelector("#choose-grid");
-const GRID_CONTAINER_WIDTH = GRID_CONTAINER.offsetWidth;
+const GRID_CONTAINER_WIDTH = 320;
 
 GRID_BUTTON.addEventListener("click", () => askSize());
 
 function askSize() {
-  let customSize = parseInt(prompt("Enter size of grid, maximum 100 squares."));
+  let customSize = parseInt(
+    prompt("Enter size of grid, maximum 100 squares.", "16")
+  );
   createNewGrid(customSize);
 }
 
@@ -23,13 +25,21 @@ function createNewGrid(size) {
           "style",
           `height:${width_height}px; width:${width_height}px;`
         );
-        div.addEventListener("mouseover", (e) => {
-          e.target.classList.add("grid-div-painted");
-        });
+        div.addEventListener("mouseover", addPaint);
+        div.addEventListener("mousedown", addPaint);
         GRID_CONTAINER.appendChild(div);
       }
     }
   }
 }
-
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+function addPaint(e) {
+  if (e.type === "mouseover" && !mouseDown) {
+    return;
+  } else {
+    e.target.classList.add("grid-div-painted");
+  }
+}
 createNewGrid(16);
